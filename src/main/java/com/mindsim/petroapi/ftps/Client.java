1c7bf21c34ba1b862bd9ba6b46eccf30ad8de62b
enrocha0312 <enrocha0312@gmail.com>
2023-09-19 19:47:24 -0300

    ftps client

--- src/main/java/com/mindsim/petroapi/ftps/Client.java
+++ src/main/java/com/mindsim/petroapi/ftps/Client.java
@@ -1,4 +1,11 @@
 package com.mindsim.petroapi.ftps;
 
+import org.apache.commons.net.ftp.FTPSClient;
+
 public class Client {
+    private String server;
+    private int port;
+    private String user;
+    private String password;
+    private FTPSClient ftpsClient;
 }
